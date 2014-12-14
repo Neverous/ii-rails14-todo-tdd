2a9f3740db89e76435415abb8a731864ffcf54f2
Maciej Szeptuch (Neverous) <neverous@neverous.info>
2014-12-14 16:28:45 +0100

    added models tests

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "list requires name" do
+        list = List.new()
+        assert_not list.valid?, "Name is required"
+    end
+
+    test 'valid list' do
+        list = List.new(name: 'TestList')
+        assert list.valid?, 'Name is only required field for list'
+    end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'task requires name' do
+        list = List.new()
+        task1 = Task.new()
+        task2 = Task.new(done: true)
+        task3 = Task.new(list: list)
+        assert_not task1.valid? || task2.valid? || task3.valid?, 'Name is required'
+    end
+
+    test 'task requires list' do
+        task = Task.new(name: 'test')
+        assert_not task.valid?, 'List is required'
+    end
+
+    test 'list should exist' do
+        task = Task.new(name: 'TestList', done: false, list_id: 777)
+        assert_not task.valid?, 'Valid list is required'
+    end
+
+    test 'valid task' do
+        list = List.new(name: 'ListTest')
+        task = Task.new(name: 'TaskTest', done: false, list: list)
+        assert task.valid?, 'Name and list are only required fields'
+    end
 end
